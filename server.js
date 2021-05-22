3382888e97fbfee58400ca3a3a70af193d172f70
japankid-code <japankid.jake@gmail.com>
2021-05-22 13:07:31 -0500

    set up MVC structure, add homepage template and close #6

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// takes all static content and serves as assets
+app.use(express.static(path.join(__dirname, './public')));
 
 // turn on routing from ./routes/index.js
 app.use(routes);
+
 
 // turn on db connection thru sequelize
 sequelize.sync({ force: false }).then(() => {
