d5661550e53cc300fa298b0dc62689cdf6bb23ea
japankid-code <japankid.jake@gmail.com>
2021-05-23 14:13:47 -0500

    users can now log in and out on the site

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -59,7 +59,14 @@ router.post('/', (req, res) => {
     email: req.body.email,
     password: req.body.password
   })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.usename = dbUserData.username;
+        req.session.loggedIn = true;
+        res.json(dbUserData);
+      })
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -81,7 +88,12 @@ router.post('/login', (req, res) => {
       res.status(400).json({ message: 'Wrong password!'});
       return;
     }
-    res.json({ use: dbUserData, message: 'You are now logged in! :)' });
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.usename = dbUserData.username;
+      req.session.loggedIn = true;
+      res.json({ use: dbUserData, message: 'You are now logged in! :)' });
+    })
   })
 })
 
@@ -121,6 +133,17 @@ router.delete('/:id', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,10 +4,24 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const PORT = process.env.PORT || 3001;
+const secret = process.env.SECRET
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const sess = {
+  secret: secret,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
