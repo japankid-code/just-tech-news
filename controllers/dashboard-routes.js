625ac999131f8127ce0a99c6a33233a8084f00c2
japankid-code <japankid.jake@gmail.com>
2021-05-24 13:04:58 -0500

    users can now update and delete posts, close #9

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -38,7 +38,52 @@ router.get('/', withAuth, (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
-
 })
+
+router.get('/edit/:id', withAuth, (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+
+      const post = dbPostData.get({ plain: true });
+      // pass data to template here
+      res.render('edit-post', {
+        post,
+        loggedIn: true
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
